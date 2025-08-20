8cf1e39e863d64057bdec5128303733ee30b8449
bielkeesu <bilkisumuhammad57@gmail.com>
2025-08-20 15:34:35 +0100

    app changes

--- app/src/utils/health.js
+++ app/src/utils/health.js
@@ -1,9 +1,11 @@
-import API_BASE_URL from "./apiConfig";
+import {getApiUrl} from "./apiConfig";
 
 // src/api/health.js
 export async function checkHealth() {
     try {
-      const res = await fetch(`${API_BASE_URL}/health`); // replace with your backend URL
+      const apiUrl = await getApiUrl();
+
+      const res = await fetch(`${apiUrl}/health`); // replace with your backend URL
       if (!res.ok) throw new Error("Server not responding");
   
       const data = await res.json();
